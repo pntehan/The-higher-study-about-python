46394165b71a7895c2f89dd128cf61ca837399ac
pntehan <2390003207@qq.com>
2019-03-12 18:34:14 +0800

    python 生成器同步协程开发

---
+++ coroutine_test.py
@@ -0,0 +1,18 @@
+# # 生成器的同步协程开发
+#
+# def gen_func():
+# 	html = yield 'http://www.baidu.com'
+# 	print(html)
+# 	yield 1
+# 	yield 2
+#
+# if __name__ == '__main__':
+# 	gen = gen_func()
+# 	url = next(gen)
+# 	print(url)
+# 	gen.send('What fuck it is?')
+
+# yield from解析
+# 观看视频
+
+
